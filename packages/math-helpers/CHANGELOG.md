2208304463f259357687094bcbc56a4246858a14
metacode22 <alohajune22@gmail.com>
2024-01-09 17:59:29 +0900

    feat: metamong-math-helper 패키지 생성

---
+++ packages/math-helpers/CHANGELOG.md
@@ -0,0 +1,19 @@
+# metamong-math-helpers
+
+## 1.0.3
+
+### Patch Changes
+
+- insert jsDoc
+
+## 1.0.2
+
+### Patch Changes
+
+- test
+
+## 1.0.1
+
+### Patch Changes
+
+- create
---
+++ packages/math-helpers/src/index.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param a
+ * @param b
+ * @returns a + b
+ */
+export function add(a: number, b: number) {
+  return a + b;
+}
+
+/**
+ *
+ * @param a
+ * @param b
+ * @returns a - b
+ */
+export function subtract(a: number, b: number) {
+  return a - b;
+}
